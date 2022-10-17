be6570a3b54d50f81505f92b4582a89ab385c0e0
Josef Lambley <17LamblJ@fulford.york.sch.uk>
2022-10-17 10:39:40 +0100

    Add project files.

---
+++ Muisc maschine/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Muisc_maschine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
